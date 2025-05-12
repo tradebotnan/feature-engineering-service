589920a7713c7bfb501e8244cbc5820619e18bd0
nanda <nandagiri.aditya@gmail.com>
2025-05-12 10:30:17 -0500

    v3. Needs refactoring.

---
+++ app/constants/indicator_types.py
@@ -0,0 +1,19 @@
+# constants/indicator_types.py
+
+INDICATOR_TYPES = {
+    "momentum_indicators": "momentum_indicators",
+    "trend_indicators": "trend_indicators",
+    "volatility_indicators": "volatility_indicators",
+    "volume_indicators": "volume_indicators",
+    "options_indicators": "options_indicators",
+    "sentiment_analysis": "sentiment_analysis",
+    "cross_features": "cross_features",
+    "time_features": "time_features",
+    "event_flags": "event_flags.py",
+    "fundamentals": "fundamentals.py",
+    "accumulation_distribution": "accumulation_distribution",
+    "chaikin_money_flow": "chaikin_money_flow",
+    "ichimoku": "ichimoku.py",
+    "donchian_channel": "donchian_channel",
+    "engineered_features": "engineered_features",
+}
